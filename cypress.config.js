95e8020ee5ebed9116fcaa87a13a832c8e6522fa
owisqari <owisbukhari@gmail.com>
2026-01-21 19:56:19 +0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "vstkfp",
-  video: true,
   reporter: "mochawesome",
   reporterOptions: {
     reportDir: "cypress/reports",
@@ -13,8 +11,5 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.js",
-    setupNodeEvents(on, config) {
-      return config;
-    },
   },
 });
